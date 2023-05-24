fb743f8a24509fb0b1af80a7be1adc3e5dd002ca
Capricorn <Capricorn@DESKTOP-DBG7PJB>
2023-05-24 17:29:30 +0530

    Add project files.

---
+++ FolderDirectory/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace FolderDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"D:\Ravi Ranjan");
+            FileInfo[] f = d.GetFiles("*", SearchOption.TopDirectoryOnly);
+            //FileInfo[] f = d.GetFiles("*", SearchOption.AllDirectories);
+            Console.WriteLine(f.Length.ToString());
+            Console.ReadLine();
+        }
+    }
+}
